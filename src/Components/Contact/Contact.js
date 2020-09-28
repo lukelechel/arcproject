c827ea376162f9b0b8b4a57b0ad39a85a7ae8bd9
Luke Lechel <13129882+lukelechel@users.noreply.github.com>
2020-09-28 15:42:58 -0400

    Easy mobile fixes

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -24,23 +24,23 @@ const Contact = () => {
                     <div id="contactFormContainer">
                         <h2 className='tallSectionLead'>Contact Us</h2>
                         <form id="contactUsForm">
-                            <label for="name">Name</label>
+                            <label htmlFor="name">Name</label>
                             <input type="text" name="name"></input>
-                            <label for="email">Email</label>
+                            <label htmlFor="email">Email</label>
                             <input type="text" name="email"></input>
-                            <label for="phone">Phone</label>
+                            <label htmlFor="phone">Phone</label>
                             <input type="text" name="phone"></input>
-                            <label for="message">Message</label>
+                            <label htmlFor="message">Message</label>
                             <textarea name="message" rows="" cols="70"></textarea>
-                            <label for="capcha">Capcha</label>
+                            <label htmlFor="capcha">Capcha</label>
                             <img src="../../resources/imgs/capcha.png" alt="ReCapcha" id="capcha" />
                             <input type="submit" value="Submit"></input>
                         </form>
                     </div>
                     <div id="serviceLocationsContainer">
                         <h2 className="tallSectionLead">Service Locations</h2>
-                        <p>Lafayette, IN: 765-701-8496</p>
-                        <p>Fort Wayne, IN: 260-760-1387</p>
+                        <p>Lafayette, IN: <b>765-701-8496</b></p>
+                        <p>Fort Wayne, IN: <b>260-760-1387</b></p>
                         <h3>WE OFFER OUR SERVICES FROM LAFAYETTE, IN TO FORT WAYNE, IN</h3>
                         <p>We’re always available by phone Monday-Friday from 8 AM-5 PM to help with your window treatment and window tinting projects.</p>
                         <p>Schedule an appointment today to get the process started!</p>
